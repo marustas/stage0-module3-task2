6f50848fe1042ee3a5a1625613943be3ec45172e
Stanislau Marudau <marudov.stas@gmail.com>
2022-06-26 22:58:45 +0300

    Fifth commit to the third task of mjc course

--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -3,13 +3,13 @@ package lang.print.gaps.task2;
 public class NamingConvention {
     public static void main(String[] args) {
         int variable = 10;
-        int variable_one = 1;
+        int variableOne = 1;
         int variableTwo = 2;
-        int variable_three = 3;
+        int variableThree = 3;
         int variableFour = 4;
         int variableFive = 5;
         int VariableSix = 6;
-        int variableSeven = 9;
+        int variableSeven = 7;
         int VariableEight = 8;
     }
 }
